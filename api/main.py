deaa896ac45334d01a65f888a5545d75f0dae9b4
Andrew Desousa <andrew.desousa@tum.de>
2020-12-06 19:38:43 +0100

    API with model deployed

---
+++ api/main.py
@@ -0,0 +1,34 @@
+import os
+import cv2
+import zipfile
+from fastapi import FastAPI, File
+from fastapi.responses import FileResponse
+from api.ml.model import setup_cfg, VisualizationDemo
+
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict(img: bytes = File(...)):
+
+    # Clear old files and save input image
+    os.system('rm response.zip && rm -rf output && mkdir output')
+    input_img = open(f'output/input.png', "wb")
+    input_img.write(img)
+    input_img.close()
+    
+    
+    cfg = setup_cfg()
+    demo = VisualizationDemo(cfg, vis_highest_scoring=False, output_dir='output')
+    
+    input_img = cv2.imread('output/input.png')
+    predictions = demo.run_on_image(input_img)
+
+    with zipfile.ZipFile('response.zip', 'w') as myzip:
+        for i in os.listdir('output'):
+            myzip.write(f'output/{i}')
+
+    response = FileResponse(path='response.zip', filename='response.zip')
+    return response
+
+    
