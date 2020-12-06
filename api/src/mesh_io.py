ef9ccae8dd4bb1407114d0c4442d9b40a8fac738
Ugurcan Polat <ugurcan.polat@tum.de>
2020-12-06 17:03:01 +0100

    Update mesh IO and texturing scripts

--- api/src/mesh_io.py
+++ api/src/mesh_io.py
@@ -1,9 +1,10 @@
 import os
 
 from cv2 import imwrite
+from texturing import MeshTextureType
 
 
-def write_object(output_path, vertices, faces, mask, det_id=1):
+def write_object(output_path, vertices, texturing, mask, det_id=1):
     filename = "obj_{}".format(det_id)
     mask_filepath = os.path.join(output_path, filename + ".png")
     obj_filepath = os.path.join(output_path, filename + ".obj")
@@ -11,12 +12,33 @@ def write_object(output_path, vertices, faces, mask, det_id=1):
 
     imwrite(mask_filepath, mask)
 
-    # Texture material
+    # obj write
     with open(obj_filepath, 'w') as file:
         file.write("mtllib {}".format(mtl_filepath.split("/")[-1]) + os.linesep)
 
         for v in vertices:
             file.write("v {} {} {}".format(v[0], v[1], v[3]) + os.linesep)
 
-        for f in faces:
-            file.write("f {} {} {}".format(f[0], f[1], f[3]) + os.linesep)
+        for i in range(len(texturing.textures)):
+            file.write("usemtl material_{}".format(i) + os.linesep)
+            for f in texturing.textures[i].faces:
+                file.write("f {} {} {}".format(f[0], f[1], f[3]) + os.linesep)
+
+    # mtl write
+    with open(mtl_filepath, 'w') as file:
+        for i in range(len(texturing.textures)):
+            file.write("newmtl material_{}".format(i) + os.linesep)
+            texture_data = texturing.textures[i].texture_data
+
+            if texturing.textures[i].type == MeshTextureType.IMAGE_TEXTURE:
+                # TODO: Create image mapping, append to obj file
+                file.write("map_Kd {}".format(texture_data[0]) + os.linesep)
+            elif texturing.textures[i].type == MeshTextureType.AVG_COLOR:
+                file.write("Kd {} {} {}".format(texture_data[0][0], texture_data[0][1],
+                                                texture_data[0][2]) + os.linesep)
+                file.write("Ks {} {} {}".format(texture_data[1][0], texture_data[1][1],
+                                                texture_data[1][2]) + os.linesep)
+                file.write("Ns {}".format(texture_data[2]) + os.linesep)
+            else:
+                # TODO: Add a error log
+                pass
--- api/src/texturing.py
+++ api/src/texturing.py
@@ -1,6 +1,37 @@
 class Texturing:
-    def assign_average_color(self):
+    def __init__(self, all_faces, mask):
+        self.all_faces = all_faces
+        self.mask = mask
+        self.textures = []
+
+    def _create_average_color_texture(self):
+        avg_color = [0, 0, 0]
+
+        for p in self.mask:
+            avg_color += p
+
+        avg_color /= (len(self.mask) * 255)
+
+        # TODO: Put coloring constants in a constants file
+        ks = [0.50, 0.50, 0.50]
+        ns = [18.00]
+
+        texture_data = [avg_color, ks, ns]
+        avg_color_texture = MeshTexture(MeshTextureType.AVG_COLOR, texture_data)
+
+        self.textures.append(avg_color_texture)
+
+    # TODO: Implement image projection texturing
+    def _create_image_projection_texture(self):
         return NotImplementedError
 
-    def assign_image_projection(self):
-        return NotImplementedError
+
+class MeshTextureType:
+    AVG_COLOR = "avg_color"
+    IMAGE_TEXTURE = "image_texture"
+
+
+class MeshTexture:
+    def __init__(self, texture_type, texture_data):
+        self.type = texture_type
+        self.texture_data = texture_data
